6961e9221d6c41d5bead5bf3ae46da5b9a3a3aec
gaskeo <zeytunyan2004@mail.ru>
2023-03-04 16:48:31 +0500

    fix(ui): new example grid

--- widgets/exampleWidget/ui/exampleWidget.tsx
+++ widgets/exampleWidget/ui/exampleWidget.tsx
@@ -12,92 +12,109 @@ interface ExampleWidgetProps {
 
 export const ExampleWidget = ({className}: ExampleWidgetProps) => {
     return (
-        <Grid container columns={{xs: 1, sm: 1, md: 1, lg: 4, xl: 4}} gap={4} className={className}>
-            <Grid item xs={2}>
-                <Box display="flex" flexDirection="column" gap={2}>
-                    <Grid container columns={{xs: 2, sm: 2, md: 4, lg: 4, xl: 4}} gap={2}>
-                        <Grid item xs>
-                            <Button type="default">Кнопка</Button>
-                        </Grid>
-                        <Grid item xs>
-                            <Button type="success">Кнопка</Button>
-                        </Grid>
-                        <Grid item xs>
-                            <Button type="default" fill={false}>Кнопка</Button>
-                        </Grid>
-                        <Grid item xs>
-                            <Button type="success" fill={false}>Кнопка</Button>
-                        </Grid>
+        <Box className={className}>
+            <Box
+                display="inline-block"
+            >
+                {/* buttons */}
+                <Box display="flex" gap={2} flexWrap="wrap">
+                    <Grid item xs>
+                        <Button type="default">Кнопка</Button>
                     </Grid>
-                    <Box display="flex" gap={2} justifyContent="space-between" flexDirection="column">
-                        <Box display="grid" gridTemplateColumns="1fr 1fr 1fr" gap={1}>
-                            <Header size="l">Text</Header>
-                            <Header size="l" type="defaultLight">Text</Header>
-                            <Header size="l" type="success">Text</Header>
-                        </Box>
-                        <Box display="grid" gridTemplateColumns="1fr 1fr 1fr" gap={1}>
-                            <Header size="m">Text</Header>
-                            <Header size="m" type="defaultLight">Text</Header>
-                            <Header size="m" type="success">Text</Header>
-                        </Box>
-                        <Box display="grid" gridTemplateColumns="1fr 1fr 1fr" gap={1}>
-                            <Header size="s">Text</Header>
-                            <Header size="s" type="defaultLight">Text</Header>
-                            <Header size="s" type="success">Text</Header>
-                        </Box>
+                    <Grid item xs>
+                        <Button type="success">Кнопка</Button>
+                    </Grid>
+                    <Grid item xs>
+                        <Button type="default" fill={false}>Кнопка</Button>
+                    </Grid>
+                    <Grid item xs>
+                        <Button type="success" fill={false}>Кнопка</Button>
+                    </Grid>
+                </Box>
+
+                {/* Headers */}
+                <Box
+                    display="grid"
+                    gap={2}
+                    gridTemplateRows="2fr 2fr 1fr"
+                    alignItems="end"
+                >
+                    <Box display="grid" gridTemplateColumns="1fr 1fr 1fr" gap={1}>
+                        <Header size="l">Text</Header>
+                        <Header size="l" type="defaultLight">Text</Header>
+                        <Header size="l" type="success">Text</Header>
                     </Box>
-                    <Box display="flex" gap={2} justifyContent="space-between" flexDirection="column">
-                        <Box display="grid" gridTemplateColumns="1fr 1fr 1fr" gap={1}>
-                            <Text size="l">Text</Text>
-                            <Text size="l" type="defaultLight">Text</Text>
-                            <Text size="l" type="success">Text</Text>
-                        </Box>
-                        <Box display="grid" gridTemplateColumns="1fr 1fr 1fr" gap={1}>
-                            <Text size="m">Text</Text>
-                            <Text size="m" type="defaultLight">Text</Text>
-                            <Text size="m" type="success">Text</Text>
-                        </Box>
-                        <Box display="grid" gridTemplateColumns="1fr 1fr 1fr" gap={1}>
-                            <Text size="s">Text</Text>
-                            <Text size="s" type="defaultLight">Text</Text>
-                            <Text size="s" type="success">Text</Text>
-                        </Box>
+                    <Box display="grid" gridTemplateColumns="1fr 1fr 1fr" gap={1}>
+                        <Header size="m">Text</Header>
+                        <Header size="m" type="defaultLight">Text</Header>
+                        <Header size="m" type="success">Text</Header>
+                    </Box>
+                    <Box display="grid" gridTemplateColumns="1fr 1fr 1fr" gap={1}>
+                        <Header size="s">Text</Header>
+                        <Header size="s" type="defaultLight">Text</Header>
+                        <Header size="s" type="success">Text</Header>
                     </Box>
                 </Box>
-            </Grid>
-            <Grid item xs={1}>
-                <Box>
-                    <Box display="flex" flexDirection="column" gap={2} width="max-content">
+
+                {/* Text */}
+                <Box
+                    display="grid"
+                    gap={2}
+                    gridTemplateRows="2fr 2fr 1fr"
+                    alignItems="end"
+                >
+                    <Box display="flex" gridTemplateColumns="1fr 1fr 1fr" gap={1}>
+
+                        <Text size="l">Text</Text>
+                        <Text size="l" type="defaultLight">Text</Text>
+                        <Text size="l" type="success">Text</Text>
+                    </Box>
+                    <Box display="grid" gridTemplateColumns="1fr 1fr 1fr" gap={1}>
+                        <Text size="m">Text</Text>
+                        <Text size="m" type="defaultLight">Text</Text>
+                        <Text size="m" type="success">Text</Text>
+                    </Box>
+                    <Box display="grid" gridTemplateColumns="1fr 1fr 1fr" gap={1}>
+                        <Text size="s">Text</Text>
+                        <Text size="s" type="defaultLight">Text</Text>
+                        <Text size="s" type="success">Text</Text>
+                    </Box>
+                </Box>
+
+                <Box display="flex" flexDirection="column" gap={2} width="350px">
+                    <form>
                         <Input
                             label="Поле ввода телефона"
                             hint="Телефон должен содержать 11 цифр"
                             type="phone"
                             placeholder="999-111-11-11"/>
+                    </form>
+                    <form>
                         <Input label="Обычное поле ввода" hint="Имя, например" placeholder="Иван"/>
                         <Input label="Поле ввода пароля" hint="Придумайте сложный пароль" type="password"
                                placeholder="Пароль"/>
+                    </form>
+                </Box>
+                <Box display="flex" flexDirection="column" gap={2} width="350px">
 
-                        <Box display="flex" gap={1} alignItems="baseline">
-                            <Link href="#" size="l">Ссылка</Link>
-                            <Link href="#">Ссылка</Link>
-                            <Link href="#" size="s">Ссылка</Link>
-                        </Box>
-                        <Box display="flex" gap={1} alignItems="baseline">
-                            <Link href="#" size="l" type="defaultLight">Ссылка</Link>
-                            <Link href="#" type="defaultLight">Ссылка</Link>
-                            <Link href="#" size="s" type="defaultLight">Ссылка</Link>
-                        </Box>
+                    <Box display="flex" gap={1} alignItems="baseline">
+                        <Link href="#" size="l">Ссылка</Link>
+                        <Link href="#">Ссылка</Link>
+                        <Link href="#" size="s">Ссылка</Link>
+                    </Box>
+                    <Box display="flex" gap={1} alignItems="baseline">
+                        <Link href="#" size="l" type="defaultLight">Ссылка</Link>
+                        <Link href="#" type="defaultLight">Ссылка</Link>
+                        <Link href="#" size="s" type="defaultLight">Ссылка</Link>
                     </Box>
                 </Box>
-            </Grid>
-            <Grid item xs={1}>
-                <Box>
+                <Box display="flex" gap={2}>
                     <Checkbox size="l"/>
                     <Checkbox/>
                     <Checkbox size="s" type="defaultLight"/>
                 </Box>
-            </Grid>
-        </Grid>
+            </Box>
+        </Box>
     );
 };
 
--- widgets/exampleWidget/ui/styles.ts
+++ widgets/exampleWidget/ui/styles.ts
@@ -2,8 +2,27 @@ import styled from "styled-components";
 import {ExampleWidget} from "./exampleWidget";
 
 export const ExampleWidgetStyled = styled(ExampleWidget)`
-  max-width: 80vw;  
-  width: max-content;
-  border: 1px solid black;
-  padding: 16px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  min-height: 100vh;
+  overflow: auto;
+  display: flex;
+  
+  & > * > * {
+    display: inline-block;
+    vertical-align: top;
+    margin: 30px;
+    height: max-content;
+    max-width: 500px;
+    
+    & > * {
+      margin-top: 20px;
+    }
+    
+    & > form > * {
+      margin-top: 20px;
+    }
+  }
 `;
