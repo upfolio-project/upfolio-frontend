5df60d5bf7f5417025b87551961afefd04ce6705
Artyom <zeytunyan2004@mail.ru>
2023-03-04 09:44:42 +0500

    fix(build): pages/_home/ui/styles.ts to styles.tsx

---
+++ pages/_home/ui/styles.tsx
@@ -0,0 +1,13 @@
+import Home from "./home";
+import styled from "styled-components";
+
+export const HomeStyled = styled(Home)`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const _ = () => <></>;
+export default _;
