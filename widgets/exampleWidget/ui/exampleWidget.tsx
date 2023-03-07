ef65e0c8e4d7afa7fcfb212697118d6e31691395
gaskeo <zeytunyan2004@mail.ru>
2023-03-07 16:44:05 +0500

    feat(pages): change text examples in /_home/ui page

--- widgets/exampleWidget/ui/exampleWidget.tsx
+++ widgets/exampleWidget/ui/exampleWidget.tsx
@@ -54,19 +54,19 @@ export const ExampleWidget = ({className}: ExampleWidgetProps) => {
                     alignItems="end"
                 >
                     <Box display="grid" gridTemplateColumns="1fr 1fr 1fr" gap={1}>
-                        <Header size="l">Text</Header>
-                        <Header size="l" type="defaultLight">Text</Header>
-                        <Header size="l" type="success">Text</Header>
+                        <Header size="l">Текст</Header>
+                        <Header size="l" type="defaultLight">Текст</Header>
+                        <Header size="l" type="success">Текст</Header>
                     </Box>
                     <Box display="grid" gridTemplateColumns="1fr 1fr 1fr" gap={1}>
-                        <Header size="m">Text</Header>
-                        <Header size="m" type="defaultLight">Text</Header>
-                        <Header size="m" type="success">Text</Header>
+                        <Header size="m">Текст</Header>
+                        <Header size="m" type="defaultLight">Текст</Header>
+                        <Header size="m" type="success">Текст</Header>
                     </Box>
                     <Box display="grid" gridTemplateColumns="1fr 1fr 1fr" gap={1}>
-                        <Header size="s">Text</Header>
-                        <Header size="s" type="defaultLight">Text</Header>
-                        <Header size="s" type="success">Text</Header>
+                        <Header size="s">Текст</Header>
+                        <Header size="s" type="defaultLight">Текст</Header>
+                        <Header size="s" type="success">Текст</Header>
                     </Box>
                 </Box>
 
@@ -78,19 +78,19 @@ export const ExampleWidget = ({className}: ExampleWidgetProps) => {
                     alignItems="end"
                 >
                     <Box display="flex" gridTemplateColumns="1fr 1fr 1fr" gap={1}>
-                        <Text size="l">Text</Text>
-                        <Text size="l" type="defaultLight">Text</Text>
-                        <Text size="l" type="success">Text</Text>
+                        <Text size="l">Текст</Text>
+                        <Text size="l" type="defaultLight">Текст</Text>
+                        <Text size="l" type="success">Текст</Text>
                     </Box>
                     <Box display="grid" gridTemplateColumns="1fr 1fr 1fr" gap={1}>
-                        <Text size="m">Text</Text>
-                        <Text size="m" type="defaultLight">Text</Text>
-                        <Text size="m" type="success">Text</Text>
+                        <Text size="m">Текст</Text>
+                        <Text size="m" type="defaultLight">Текст</Text>
+                        <Text size="m" type="success">Текст</Text>
                     </Box>
                     <Box display="grid" gridTemplateColumns="1fr 1fr 1fr" gap={1}>
-                        <Text size="s">Text</Text>
-                        <Text size="s" type="defaultLight">Text</Text>
-                        <Text size="s" type="success">Text</Text>
+                        <Text size="s">Текст</Text>
+                        <Text size="s" type="defaultLight">Текст</Text>
+                        <Text size="s" type="success">Текст</Text>
                     </Box>
                 </Box>
 
