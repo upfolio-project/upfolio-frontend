c82689225db11d26d00090caa4e3f28d6e42b06a
gaskeo <zeytunyan2004@mail.ru>
2023-03-08 16:23:35 +0500

    feat(seo): add tags on _app and user pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,30 @@ import "@/styles/globals.scss";
 import type {AppProps} from "next/app";
 import {setupStore} from "@/shared/store";
 import {Provider} from "react-redux";
+import Head from "next/head";
+import {DefaultTags, Meta} from "@/shared/seo";
+import React from "react";
 
-export default function App({Component, pageProps}: AppProps) {
+interface MyAppProps {
+    meta?: Meta
+}
+
+export default function App({Component, pageProps}: AppProps<MyAppProps>) {
     const store = setupStore();
+
+    const meta  = pageProps.meta?.tags || [];
+    const title = pageProps.meta?.title || "UpFolio";
+
     return (
-        <Provider store={store}>
-            <Component {...pageProps} />
-        </Provider>
+        <>
+            <Head>
+                {...DefaultTags}
+                {meta && meta.map(prop => <meta {...prop} key={prop.key}/>)}
+                <title key="title">{title}</title>
+            </Head>
+            <Provider store={store}>
+                <Component {...pageProps} />
+            </Provider>
+        </>
     );
 }
