1cdd48fc987cb39c90ea9e5035fb9d87ccf44bd8
dowlsss <dowlssss@yandex.ru>
2023-03-15 14:17:11 +0500

    ui(logo): added the ability to use a single logo text

--- shared/ui/logo/styles/logo.tsx
+++ shared/ui/logo/styles/logo.tsx
@@ -1,47 +1,56 @@
 import styled from "styled-components";
-import logo from "@/shared/images/logo/logoSizeS.png";
+import logoWithoutTextSizeS from "@/shared/images/logo/logoSizeS.png";
+import logoWithTextSizeS from "@/shared/elements/logo/logoWithTextSizeS.svg";
 import Image, {StaticImageData} from "next/image";
 
 interface LogoProps {
     size: 's' | 'm'
+    withText?: boolean
+}
+
+function getSrc(withText: boolean | undefined, logoWithText: StaticImageData | string, logoWithoutText: StaticImageData | string) {
+    return withText ? logoWithText : logoWithoutText;
 }
 
 export const Logo = (props: LogoProps) => {
     switch (props.size) {
         case "s":
-            return <LogoContainer dimension='30px' src={logo} alt=''/>;
+            return <LogoContainer width='125px' height='30px'
+                                  src={getSrc(props.withText, logoWithTextSizeS, logoWithoutTextSizeS)} alt=''/>;
         case "m":
-            return <LogoContainer dimension='30px' src={logo} alt=''/>;
+            return <LogoContainer width='125px' height='30px'
+                                  src={getSrc(props.withText, logoWithTextSizeS, logoWithoutTextSizeS)} alt=''/>;
         default:
             return null;
     }
 };
 
 interface LogoContainerProps {
-    dimension: string
+    width: string
+    height: string
     src: StaticImageData | string
     alt: string
 }
 
-const LogoContainer = ({dimension, src, alt}: LogoContainerProps) => {
+const LogoContainer = ({width, height, src, alt}: LogoContainerProps) => {
     return (
-        <ImgWrapper dimension={dimension}>
+        <ImgWrapper width={width} height={height}>
             <ImgStyled src={src} alt={alt}/>
         </ImgWrapper>
     );
 };
 
 const ImgStyled = styled(Image)`
-  aspect-ratio: 1/1;
   width: 100%;
   height: auto;
 `;
 
 interface ImgWrapperProps {
-    dimension: string
+    width: string
+    height: string
 }
 
 const ImgWrapper = styled('div')<ImgWrapperProps>`
-  width: ${props => props.dimension};
-  height: ${props => props.dimension};
+  width: ${props => props.width};
+  height: ${props => props.height};
 `;
