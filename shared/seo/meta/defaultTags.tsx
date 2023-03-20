53444be9168c55c3f13f0323ff262acbea91347f
gaskeo <zeytunyan2004@mail.ru>
2023-03-20 09:27:15 +0500

    fix(favicon): change main `rel="icon"` link from .ico to .svg for yandex search engine

--- shared/seo/meta/defaultTags.tsx
+++ shared/seo/meta/defaultTags.tsx
@@ -2,9 +2,9 @@ import logo from "@/public/logo968x504.png";
 import React from "react";
 
 export const DefaultTags = [
-    <link key="favicon" rel="icon" href="/favicon.ico" sizes="any" type="image/x-icon"/>,
-    <link key="icon" rel="icon" href="/icon.svg" type="image/svg+xml"/>,
-    <link key="maskIcon" rel="mask-icon" href="/icon.svg" color="#bbf077"/>,
+    <link key="favicon" rel="shortcut icon" href="/favicon.ico" sizes="any" type="image/x-icon"/>,
+    <link key="icon" rel="icon" href="/favicon.svg" type="image/svg+xml"/>,
+    <link key="maskIcon" rel="mask-icon" href="/favicon.svg" color="#bbf077"/>,
     <link key="appleTouchIcon" rel="apple-touch-icon" href="/apple-touch-icon.png"/>,
     <link key="manifest" rel="manifest" href="/manifest.webmanifest"/>,
 
