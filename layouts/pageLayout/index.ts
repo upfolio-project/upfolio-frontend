837950c43b34230053ab0c21eccc9638c3234717
gaskeo <zeytunyan2004@mail.ru>
2023-03-25 00:11:44 +0500

    layout(user page): new layout for user's pages with `margin-top: 120px`

---
+++ layouts/pageLayout/index.ts
@@ -0,0 +1,3 @@
+import {PageLayout} from "./ui/pageLayout";
+
+export {PageLayout};
---
+++ layouts/pageLayout/ui/pageLayout.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {Header} from "@/widgets/headerWidget";
+import {FooterWidget} from "@/widgets/footerWidget";
+import styled from "styled-components";
+
+const Main = styled.main`
+  box-sizing: border-box;
+  padding-top: 120px;
+  padding-bottom: 160px;
+  min-height: 100vh;
+  height: calc(100% + 200px);
+  width: 100vw;
+`;
+
+interface BaseLayoutProps {
+    children: React.ReactNode
+}
+
+
+export const PageLayout = ({children}: BaseLayoutProps) => {
+    return (
+        <>
+            <Header/>
+            <Main>
+                {children}
+            </Main>
+            <FooterWidget/>
+        </>
+    );
+};
