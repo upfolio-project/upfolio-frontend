acbab301d04d0275d30f415a3304fde9e2cee8ee
gaskeo <zeytunyan2004@mail.ru>
2023-04-06 15:35:58 +0500

    feat(ui): add tag and tags components

---
+++ shared/ui/tag/styles/tags.tsx
@@ -0,0 +1,24 @@
+import {Box} from "@mui/material";
+import {sizes} from "@/styles/variables";
+import {Tag} from "@/shared/ui/tag/styles/tag";
+import React from "react";
+
+const Tags = ({tags}: { tags: string[] | undefined }) => {
+    if (!tags || !tags.length) {
+        return <></>;
+    }
+    return (
+        <Box
+            display="flex"
+            flexWrap="wrap"
+            justifyContent="center"
+            gap={sizes.xxs}
+        >
+            {tags && tags.map((tag) => (
+                <Tag key={tag} value={tag} link="#"/>
+            ))}
+        </Box>
+    );
+};
+
+export {Tags};
