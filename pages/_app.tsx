09d5a3aa1e227c961bb3d6594877d57e95efc2a9
dowlsss <dowlssss@yandex.ru>
2023-04-07 15:20:36 +0500

    chore(import): imported styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import "@/shared/styles/globals.scss";
+import "@/shared/styles/globals.css";
 import type {AppProps} from "next/app";
 import {setupStore} from "@/shared/store";
 import {Provider} from "react-redux";
