0a79c6f901720c01d3235aad8509ef42d4ba3f2f
gaskeo <zeytunyan2004@mail.ru>
2023-04-12 18:24:12 +0500

    fix(skeleton): fix profile skeleton

--- widgets/profileWidget/ui/skeleton.tsx
+++ widgets/profileWidget/ui/skeleton.tsx
@@ -1,35 +1,13 @@
 import {Box, Skeleton as MUISkeleton} from "@mui/material";
 import {Header, Text} from "@/shared/ui/text";
 import styled from "styled-components";
-import {borders, colors} from "@/shared/styles";
-
-const PortfolioData = styled(Box)`
-  width: 676px;
-  border: ${borders.width2px(colors.colorSecondary05)};
-  border-radius: ${borders.radius10};
-  min-height: 120vw;
-`;
-
-const Wrapper = styled(Box)`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 30px 47px;
-  gap: 20px;
-  border: ${borders.width2px(colors.colorSecondary05)};
-  border-radius: ${borders.radius10};
-`;
+import {sizes, Wrapper } from "@/shared/styles";
 
 const UserDataStyled = styled(Box)`
-  align-self: flex-start;
-  overflow-y: auto;
-  position: sticky;
-  top: 10px;
   width: 328px;
   display: flex;
   flex-direction: column;
-  gap: 20px;
+  gap: ${sizes.s};
 `;
 
 const ContentContainer = styled(Box)`
@@ -46,7 +24,7 @@ const Skeleton = () => {
         <>
             <UserDataStyled>
                 <Wrapper>
-                    <MUISkeleton variant="circular" height="154px" width="154px"/>
+                    <MUISkeleton sx={{borderRadius: sizes.s}} variant="rounded" height="154px" width="154px"/>
                     <ContentContainer>
                         <MUISkeleton variant="text" height="24px" width="200px"/>
                         <MUISkeleton variant="text" height="16px" width="80px"/>
@@ -57,20 +35,10 @@ const Skeleton = () => {
                     </ContentContainer>
                 </Wrapper>
                 <Wrapper>
-                    <Header size="s">Обо мне</Header>
-                    <Box>
-                        <MUISkeleton variant="text" height="22px" width="200px"/>
-                        <MUISkeleton variant="text" height="22px" width="200px"/>
-                        <MUISkeleton variant="text" height="22px" width="200px"/>
-                        <MUISkeleton variant="text" height="22px" width="200px"/>
-                    </Box>
-                </Wrapper>
-                <Wrapper>
                     <Header size="s">Контакты</Header>
                     {<Text size="s" type="defaultLight">...</Text>}
                 </Wrapper>
             </UserDataStyled>
-            <PortfolioData></PortfolioData>
 
         </>
     );
