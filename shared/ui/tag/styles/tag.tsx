e3cd531f35f78a474d3e6309ea335362b0d2a046
gaskeo <zeytunyan2004@mail.ru>
2023-04-28 14:35:50 +0500

    feat(tag): add non-transparent background in tag component

--- shared/ui/tag/styles/tag.tsx
+++ shared/ui/tag/styles/tag.tsx
@@ -1,39 +1,46 @@
 import styled from "styled-components";
 import {borders, colors, sizes} from "@/shared/styles";
-import {Link} from "@/shared/ui/link";
 import React from "react";
+import Link from "next/link";
+import {Text} from "@/shared/ui/text";
 
-const TagItem = styled.span<{tagType: "default" | "accent"}>`
+const TagItem = styled(Link)<{ tagType: "default" | "accent" }>`
+  text-decoration: none;
   display: flex;
-  align-items: baseline;
+  align-items: center;
   height: ${sizes.s};
-  background-color: ${p => p.tagType === "accent" ? colors.colorAccent : colors.colorSecondary05};
+  background-color: ${p => p.tagType === "accent" ? colors.colorAccent : colors.colorSecondary05B};
   padding-left: ${sizes.xs};
   padding-right: ${sizes.xs};
   border-radius: ${borders.radius5};
+  transition: .3s;
 
   &, & * {
     color: ${p => p.tagType === "accent" ? colors.colorDominant : colors.colorAccent};
+    transition: .3s;
+  }
+  
+  & * {
+    background-color: ${colors.colorTransparent} !important;
   }
 
   &:hover, a:hover span, & span:hover {
     color: ${p => p.tagType === "accent" ? colors.colorDominant : colors.colorAccent} !important;
+    background-color: ${p => p.tagType === "accent" ? colors.colorAccent50B : colors.colorSecondary20B};
   }
 `;
 
 export interface TagProps {
-    value: React.ReactNode
-    link?: string
-    tagType?: "default" | "accent"
+    value: React.ReactNode;
+    link?: string;
+    tagType?: "default" | "accent";
 }
 
 
 const Tag = ({value, link, tagType}: TagProps) => {
     return (
-        <TagItem tagType={tagType || "default"}>
-            <Link href={link || ""} size="s"  as="span">
-                {value}
-            </Link>
+        <TagItem tagType={tagType || "default"} href={link || ""}>
+            <Text size="s" as="span">{value}</Text>
         </TagItem>
     );
 };
