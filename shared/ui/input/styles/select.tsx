1f9348a24c39bc8d58da4b047552bc1053a80dff
gaskeo <zeytunyan2004@mail.ru>
2023-04-28 14:36:20 +0500

    fix(select): fix body width when select open

--- shared/ui/input/styles/select.tsx
+++ shared/ui/input/styles/select.tsx
@@ -1,7 +1,7 @@
 import {FormControl, InputLabel, MenuItem, Select as MUISelect} from "@mui/material";
 import React, {Ref, useState} from "react";
 import styled from "styled-components";
-import {borders, colors, fonts, shadows} from "@/shared/styles";
+import {borders, colors, fonts, shadows, sizes} from "@/shared/styles";
 
 interface Item<V> {
     content: React.ReactNode;
@@ -17,7 +17,7 @@ interface SelectProps<V> {
     defaultValue?: V;
 }
 
-const Select = <V,>({items, id, label, inputRef, defaultValue}: SelectProps<V>) => {
+const Select = <V, >({items, id, label, inputRef, defaultValue}: SelectProps<V>) => {
     const [value, changeValue] = useState<V | string | number>(defaultValue || items[0].value);
     return (
         <FormControlStyled fullWidth>
@@ -26,19 +26,23 @@ const Select = <V,>({items, id, label, inputRef, defaultValue}: SelectProps<V>) 
                 inputRef={inputRef}
                 value={value}
                 onChange={(e) => changeValue(e?.target?.value as V || defaultValue || items[0].value)}
-                MenuProps={{
-                    PaperProps: {
-                        sx: {
-                            bgcolor: colors.colorDominant,
-                            borderRadius: borders.radius10,
-                            shadows: shadows.defaultShadow,
-                            marginTop: "8px"
+                inputProps={{
+                    MenuProps: {
+                        disableScrollLock: true,
+                        PaperProps: {
+                            sx: {
+                                borderRadius: borders.radius10,
+                                shadows: shadows.defaultShadow,
+                                marginTop: sizes.xs
+                            },
                         },
-                    },
+                    }
+
                 }}
                 labelId={id}
             >
-                {items.map(item => <MenuItemStyled value={item.value as string} key={item.key}>{item.content}</MenuItemStyled>)}
+                {items.map(item => <MenuItemStyled value={item.value as string}
+                                                   key={item.key}>{item.content}</MenuItemStyled>)}
             </SelectStyled>
         </FormControlStyled>
     );
@@ -47,7 +51,7 @@ const Select = <V,>({items, id, label, inputRef, defaultValue}: SelectProps<V>) 
 const MenuItemStyled = styled(MenuItem)`
   font: ${fonts.m()};
   transition: .1s all;
-  
+
   &:hover {
     background-color: ${colors.colorSecondary05};
   }
@@ -60,21 +64,21 @@ const MenuItemStyled = styled(MenuItem)`
 
 const SelectStyled = styled(MUISelect)`
   width: 100%;
-  
+
   & > div {
     border: ${borders.width2px(colors.colorAccent)} !important;
     border-radius: ${borders.radius10};
     transition: all .3s;
     height: 40px;
   }
-  
+
   & div {
     height: 40px !important;
     display: flex;
     align-items: center;
     padding: 0 18px;
     font: ${fonts.m()};
-    
+
   }
 `;
 
@@ -95,7 +99,7 @@ const FormControlStyled = styled(FormControl)`
     border: none;
   }
 
-  &:hover > div> div, &:focus-visible > div > div, &:focus-within > div > div {
+  &:hover > div > div, &:focus-visible > div > div, &:focus-within > div > div {
     border: ${borders.width2px(colors.colorAccent50)} !important;
     border-radius: ${borders.radius10} !important;
   }
